867da21242fca7d01a0049add94cff7afb5b216c
Ulzahk <ulzahk@gmail.com>
2023-03-22 05:45:19 -0500

    feat(users.service): add basic CRUD functionality for users

--- src/common/index.js
+++ src/common/index.js
@@ -6,6 +6,24 @@ function errorHandler(res, code) {
   res.end(`{"error": "${http.STATUS_CODES[code]}"}`);
 }
 
+
+function getReqData(req) {
+  return new Promise((resolve, reject) => {
+    try {
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+      req.on("end", () => {
+        resolve(body);
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
 module.exports = {
-  errorHandler
+  errorHandler,
+  getReqData
 };
--- src/users/users.service.js
+++ src/users/users.service.js
@@ -1,12 +1,65 @@
 'use strict';
+const { v4: uuidv4 } = require('uuid');
 const { users } = require('../database');
 
 class UsersService {
   constructor() { }
 
-  async getUsers() {
-    return new Promise(function (resolve, _) {
+  async findAll() {
+    return new Promise((resolve, _) => {
       resolve(users);
+    });
+  }
+
+  async findOne(id) {
+    return new Promise((resolve, reject) => {
+      let user = users.find((item) => item.id === id);
+      if (user) {
+        resolve(user);
+      } else {
+        reject(`User with id ${id} not found`);
+      }
+    });
+  }
+
+  async create(payload) {
+    return new Promise((resolve, _) => {
+      const newId = uuidv4();
+      const newUser = {
+        id: newId,
+        ...payload,
+      };
+
+      users.push(newUser);
+      resolve(newUser);
+    });
+  }
+
+  async update(id, payload) {
+    return new Promise((resolve, reject) => {
+      const user = users.find((item) => item.id === id);
+      if (!user) {
+        reject(`No user with id ${id} found`);
+      }
+      const userIndex = users.findIndex((item) => item.id === id);
+      users[userIndex] = { ...user, ...payload };
+
+      resolve(users[userIndex]);
+    });
+  }
+
+  async delete(id) {
+    return new Promise((resolve, reject) => {
+      const userIndex = users.findIndex((item) => item.id === id);
+      if (!userIndex) {
+        reject(`No user with id ${id} found`);
+      }
+
+      users.splice(userIndex, 1);
+
+      resolve({
+        message: `User with id ${id} deleted`
+      });
     });
   }
 }
